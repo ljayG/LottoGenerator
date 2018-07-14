35931ef8ed8a5ff9c3a9c289a4b9821b8f93e355
Jongwon Lee <moguri61@gmail.com>
2018-07-14 06:05:04 +0000

    hotfix2

--- src/project/LottoGenerator.java
+++ src/project/LottoGenerator.java
@@ -2,22 +2,34 @@ package project;
 
 import java.util.Random;
 import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
 
 public class LottoGenerator {
     private Random random = new Random(System.nanoTime());
     
     public int[] generate(){
-        int[] result = new int[6];
+        int[] result = new int[6]; int index=0;
+        set<Integer> generated = new HashSet<>();
         
-        for(int i = 0; i < 6; i++){
-            result[i] = random.nextInt(45) + 1;
+        while(generated.size() < 6){
+            int num = random.nextInt(45) + 1;
+        
+            // 생성된 목록에 포함되어 있지 않으면 추가
+            if(!contains(generated, num)){
+                result[index++] = num; generated.add(num);
+            }
         }
         
         return result;
     }
     
+    boolean contains(Set<Integer> generated, int num){
+        return generated.contains(num);
+    }
+    
     public static void main(String[] args) {
         int[] result = new LottoGenerator().generate();
-        System.out.printIn(Arrays.toString(result));
+        System.out.println(Arrays.toString(result));
     }
 }
